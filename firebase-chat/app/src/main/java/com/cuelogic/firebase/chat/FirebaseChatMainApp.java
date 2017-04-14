5c5c05087647e81771c81c2d3aeb9c0d32d5e89b
Swapnil S <swapnil.sonar@cuelogic.co.in>
2017-04-14 19:25:45 +0530

    Added database helper class

--- firebase-chat/app/src/main/java/com/cuelogic/firebase/chat/FirebaseChatMainApp.java
+++ firebase-chat/app/src/main/java/com/cuelogic/firebase/chat/FirebaseChatMainApp.java
@@ -1,10 +1,16 @@
 package com.cuelogic.firebase.chat;
 
 import android.app.Application;
+import android.content.Context;
+
+import com.cuelogic.firebase.chat.database.DatabaseHelper;
 
 //http://www.crazyhitty.com/one-to-one-chat-using-firebase-for-android/
 public class FirebaseChatMainApp extends Application {
 
+    private static final String TAG = FirebaseChatMainApp.class.getSimpleName();
+    private static DatabaseHelper databaseHelper;
+    private static Context mContext;
     private static boolean sIsChatActivityOpen = false;
 
     public static boolean isChatActivityOpen() {
@@ -18,5 +24,18 @@ public class FirebaseChatMainApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        databaseHelper = DatabaseHelper.getDatabaseHelper(this);
+        mContext = FirebaseChatMainApp.this;
+    }
+
+    public DatabaseHelper getDatabaseHelper() {
+        if (null == databaseHelper) {
+            databaseHelper = DatabaseHelper.getDatabaseHelper(this);
+        }
+        return databaseHelper;
+    }
+
+    public static Context getAppContext() {
+        return mContext;
     }
 }
---
+++ firebase-chat/app/src/main/java/com/cuelogic/firebase/chat/database/Sample.java
@@ -0,0 +1,36 @@
+package com.cuelogic.firebase.chat.database;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+/**
+ * @version 1.0.0
+ * @Date 14/04/2017
+ */
+@Entity
+public class Sample {
+    @Id(autoincrement = true)
+    private Long id;
+    private String userName;
+    @Generated(hash = 505417145)
+    public Sample(Long id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+    @Generated(hash = 976859954)
+    public Sample() {
+    }
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUserName() {
+        return this.userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
